601d3567a0e153a988e8e51b1b8782892354f52c
Layan Abdullah <Layanabdullah.tr@gmail.com>
2021-03-29 04:15:25 +0300

    up6

---
+++ Script/sc1.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    document.getElementById("my_audio").play();
+}
+$(function () {
+var body = $('body');
+var backgroundImages = [
+' url(Images/Mario64_3.jpeg) ',
+' url(Images/Mario64_2.jpeg) '];
+
+var current = 0;
+
+function nextBackground() {
+body.css(
+'background-image',
+backgroundImages[current = ++current % backgroundImages.length]);
+
+setTimeout(nextBackground, 10000); //automatically change background-image every 5 seconds
+}
+setTimeout(nextBackground, 10000); //automatically change background-image every 5 seconds
+body.css('background-image', backgroundImages[0]);
+});
